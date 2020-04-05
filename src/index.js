5c80b4e327a9c25a8680eeb046ed44f070a4be1b
Miles Crighton <miles.crighton@gmail.com>
2020-04-05 21:07:45 +0100

    Generating graph data from dependencies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,60 @@
 
 const fs = require("fs");
 
-let rawPackagedata = fs.readFileSync("package.json");
-let packageData = JSON.parse(rawPackagedata);
+//Get dependency list from package.json
+let rawPackageData = fs.readFileSync("package.json");
+let packageData = JSON.parse(rawPackageData);
 if (!("dependencies" in packageData)) {
     console.log("No dependencies found");
     process.exit(1);
 }
 let dependencies = packageData.dependencies;
-console.log(dependencies);
+let dependenciesKeys = Object.keys(dependencies);
+console.log(`Found dependencies: ${dependenciesKeys}`);
+
+//Go create the graph out of the package-lock.json
+let rawPackageLockData = fs.readFileSync("package-lock.json");
+let packageLockData = JSON.parse(rawPackageLockData);
+if (!("dependencies" in packageLockData)) {
+    console.log("No dependencies found");
+    process.exit(1);
+}
+let lockDeps = packageLockData.dependencies;
+let lockDepsKeys = Object.keys(lockDeps);
+//console.log(packageLockData);
+
+//*** Generate graph data ***/
+let graphData = { nodes: [], links: [] };
+let nodeMap = {};
+let idIndex = 1;
+
+//Generate nodes
+lockDepsKeys.forEach((key) => {
+    nodeMap[key] = idIndex;
+    let node = {
+        id: idIndex,
+        name: key,
+        version: lockDeps[key].version,
+    };
+    graphData.nodes.push(node);
+    idIndex++;
+});
+
+//Generate links
+lockDepsKeys.forEach((sourceKey) => {
+    if ("requires" in lockDeps[sourceKey]) {
+        let requires = lockDeps[sourceKey]["requires"];
+        let requiresKeys = Object.keys(requires);
+
+        requiresKeys.forEach((targetKey) => {
+            let link = {
+                source: nodeMap[sourceKey],
+                target: nodeMap[targetKey],
+                version: requires[targetKey],
+            };
+            graphData.links.push(link);
+        });
+    }
+});
+
+console.log(graphData);
